6e19ef0ddf54c70a8c1aeb45949d94f5a7689307
YY0NII <jonatan9102@gmail.com>
2020-11-11 18:08:16 -0500

    Setting up packages for the 4 planned games

--- app/src/main/java/com/example/loonspartypack/Game1/TicTacToe.java
+++ app/src/main/java/com/example/loonspartypack/Game1/TicTacToe.java
@@ -2,15 +2,41 @@ package com.example.loonspartypack.Game1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.loonspartypack.R;
 
 public class TicTacToe extends AppCompatActivity {
+    private boolean player1 = false;
+    private boolean player2 = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe);
+    }
+
+    public void buttonClicked(View v) {
+        if (!player1 && !player2) {
+            Button theButton = (Button) v;
+            theButton.setText("X");
+            player1 = true;
+        } else if (player1 && !player2) {
+            Button theButton = (Button) v;
+            theButton.setText("O");
+            player2 = true;
+        } else if (player1) {
+            Button theButton = (Button) v;
+            theButton.setText("X");
+            player2 = false;
+        }
+    }
+
+    public void reset(View v){
+        finish();
+        startActivity(new Intent(getIntent()));
     }
 }
--- app/src/main/java/com/example/loonspartypack/MainActivity.java
+++ app/src/main/java/com/example/loonspartypack/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.loonspartypack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.loonspartypack.Game1.TicTacToe;
+
 public class MainActivity extends AppCompatActivity {
+    Button game1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        game1 = (Button) findViewById(R.id.Game1);
+        game1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TicTacToe.class));
+            }
+        });
     }
 }
