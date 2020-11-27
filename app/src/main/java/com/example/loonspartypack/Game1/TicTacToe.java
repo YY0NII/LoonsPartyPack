5b0655ade61d80655d1016b43a12e7bf40fe11b6
YY0NII <jonatan9102@gmail.com>
2020-11-27 17:02:43 -0500

    Updated the TicTacToe activity so that it has an actual board and the buttons are no longer enabled when someone has already clicked them, or wins.

--- app/src/main/java/com/example/loonspartypack/Game1/TicTacToe.java
+++ app/src/main/java/com/example/loonspartypack/Game1/TicTacToe.java
@@ -13,19 +13,26 @@ package com.example.loonspartypack.Game1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.loonspartypack.MainActivity;
 import com.example.loonspartypack.R;
+
+import static java.lang.Boolean.FALSE;
 
 public class TicTacToe extends AppCompatActivity {
     private boolean player1 = false;
     private boolean player2 = false;
 
     Button b1, b2, b3, b4, b5, b6, b7, b8, b9;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +48,12 @@ public class TicTacToe extends AppCompatActivity {
         b7 = (Button) findViewById(R.id.button7);
         b8 = (Button) findViewById(R.id.button8);
         b9 = (Button) findViewById(R.id.button9);
+
+        textView = (TextView) findViewById(R.id.tv1);
     }
 
     //region Methods
+
     //****************************************************
     // Method: buttonClicked()
     //
@@ -52,19 +62,20 @@ public class TicTacToe extends AppCompatActivity {
     // clicked
     //****************************************************
     public void buttonClicked(View v) {
+        Button theButton = (Button) v;
+
         if (!player1 && !player2) {
-            Button theButton = (Button) v;
             theButton.setText("X");
             player1 = true;
         } else if (player1 && !player2) {
-            Button theButton = (Button) v;
             theButton.setText("O");
             player2 = true;
         } else if (player1) {
-            Button theButton = (Button) v;
             theButton.setText("X");
             player2 = false;
         }
+
+        theButton.setEnabled(false);
 
         checkIfOver();
     }
@@ -90,54 +101,120 @@ public class TicTacToe extends AppCompatActivity {
     public void checkIfOver()
     {
         //region Check For Horizontal Win
-        if ( !b1.getText().toString().isEmpty() && b1.getText().toString().equals(b2.getText().toString()) &&  b1.getText().toString().equals(b3.getText().toString()) )
+        if ( !b1.getText().toString().isEmpty() && b1.getText().toString().equals(b2.getText().toString())
+                &&  b1.getText().toString().equals(b3.getText().toString()) )
         {
-            Toast.makeText(TicTacToe.this, "Game Over", Toast.LENGTH_LONG).show();
+            checkWhoWon(b1);
+            disableButtons();
         }
-        else if ( !b4.getText().toString().isEmpty() && b4.getText().toString().equals(b5.getText().toString()) &&  b4.getText().toString().equals(b6.getText().toString()) )
+        else if ( !b4.getText().toString().isEmpty() && b4.getText().toString().equals(b5.getText().toString())
+                &&  b4.getText().toString().equals(b6.getText().toString()) )
         {
-            Toast.makeText(TicTacToe.this, "Game Over", Toast.LENGTH_LONG).show();
+            checkWhoWon(b4);
+            disableButtons();
         }
-        else if ( !b7.getText().toString().isEmpty() && b7.getText().toString().equals(b8.getText().toString()) &&  b7.getText().toString().equals(b9.getText().toString()) )
+        else if ( !b7.getText().toString().isEmpty() && b7.getText().toString().equals(b8.getText().toString())
+                &&  b7.getText().toString().equals(b9.getText().toString()) )
         {
-            Toast.makeText(TicTacToe.this, "Game Over", Toast.LENGTH_LONG).show();
+            checkWhoWon(b7);
+            disableButtons();
         }
         //endregion
 
         //region Check For Vertical  Win
-        if ( !b1.getText().toString().isEmpty() && b1.getText().toString().equals(b4.getText().toString()) &&  b1.getText().toString().equals(b7.getText().toString()) )
+        if ( !b1.getText().toString().isEmpty() && b1.getText().toString().equals(b4.getText().toString())
+                &&  b1.getText().toString().equals(b7.getText().toString()) )
         {
-            Toast.makeText(TicTacToe.this, "Game Over", Toast.LENGTH_LONG).show();
+            checkWhoWon(b1);
+            disableButtons();
         }
-        else if ( !b2.getText().toString().isEmpty() && b2.getText().toString().equals(b5.getText().toString()) &&  b2.getText().toString().equals(b8.getText().toString()) )
+        else if ( !b2.getText().toString().isEmpty() && b2.getText().toString().equals(b5.getText().toString())
+                &&  b2.getText().toString().equals(b8.getText().toString()) )
         {
-            Toast.makeText(TicTacToe.this, "Game Over", Toast.LENGTH_LONG).show();
+            checkWhoWon(b2);
+            disableButtons();
         }
-        else if ( !b3.getText().toString().isEmpty() && b3.getText().toString().equals(b6.getText().toString()) &&  b3.getText().toString().equals(b9.getText().toString()) )
+        else if ( !b3.getText().toString().isEmpty() && b3.getText().toString().equals(b6.getText().toString())
+                &&  b3.getText().toString().equals(b9.getText().toString()) )
         {
-            Toast.makeText(TicTacToe.this, "Game Over", Toast.LENGTH_LONG).show();
+            checkWhoWon(b3);
+            disableButtons();
         }
         //endregion
 
         //region Check For Diagonal  Win
-        if ( !b1.getText().toString().isEmpty() && b1.getText().toString().equals(b5.getText().toString()) &&  b1.getText().toString().equals(b9.getText().toString()) )
+        if ( !b1.getText().toString().isEmpty() && b1.getText().toString().equals(b5.getText().toString())
+                &&  b1.getText().toString().equals(b9.getText().toString()) )
         {
-            Toast.makeText(TicTacToe.this, "Game Over", Toast.LENGTH_LONG).show();
+            checkWhoWon(b1);
+            disableButtons();
         }
-        else if ( !b3.getText().toString().isEmpty() && b3.getText().toString().equals(b5.getText().toString()) &&  b3.getText().toString().equals(b7.getText().toString()) )
+        else if ( !b3.getText().toString().isEmpty() && b3.getText().toString().equals(b5.getText().toString())
+                &&  b3.getText().toString().equals(b7.getText().toString()) )
         {
-            Toast.makeText(TicTacToe.this, "Game Over", Toast.LENGTH_LONG).show();
+            checkWhoWon(b3);
+            disableButtons();
         }
         //endregion
 
         //region Check For No winner
-        if ( !b1.getText().toString().isEmpty() && !b2.getText().toString().isEmpty() && !b3.getText().toString().isEmpty() && !b4.getText().toString().isEmpty() &&
-                !b5.getText().toString().isEmpty() && !b6.getText().toString().isEmpty() && !b7.getText().toString().isEmpty() && !b8.getText().toString().isEmpty() &&
-                !b9.getText().toString().isEmpty() )
+        if ( !b1.getText().toString().isEmpty() && !b2.getText().toString().isEmpty() && !b3.getText().toString().isEmpty()
+                && !b4.getText().toString().isEmpty() && !b5.getText().toString().isEmpty() && !b6.getText().toString().isEmpty()
+                && !b7.getText().toString().isEmpty() && !b8.getText().toString().isEmpty() && !b9.getText().toString().isEmpty() )
         {
             Toast.makeText(TicTacToe.this, "Game Over", Toast.LENGTH_LONG).show();
+            disableButtons();
         }
         //endregion
     }
+
+    //****************************************************
+    // Method: disableButtons()
+    //
+    // Purpose: Disables all currently enabled buttons
+    //****************************************************
+    public void disableButtons(){
+        Button[] buttons = new Button[9];
+
+        buttons[0] = b1;
+        buttons[1] = b2;
+        buttons[2] = b3;
+        buttons[3] = b4;
+        buttons[4] = b5;
+        buttons[5] = b6;
+        buttons[6] = b7;
+        buttons[7] = b8;
+        buttons[8] = b9;
+
+        for (int i = 0; i < 9; i++){
+            if (buttons[i].isEnabled()){
+                buttons[i].setEnabled(false);
+            }
+        }
+    }
+
+    public void checkWhoWon(Button button){
+        if (button.getText().toString().equals("X")){
+            Toast.makeText(TicTacToe.this, "X Wins", Toast.LENGTH_LONG).show();
+            textView.setText(R.string.x_wins);
+        } else if (button.getText().toString().equals("O")){
+            Toast.makeText(TicTacToe.this, "O Wins", Toast.LENGTH_LONG).show();
+            textView.setText(R.string.o_wins);
+        }
+    }
+
+    //****************************************************
+    // Method: onBackPressed()
+    //
+    // Purpose: To override the default onBackPressed()
+    // so that the rest of the application is not locked
+    // to portrait mode
+    //****************************************************
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(TicTacToe.this, MainActivity.class));
+        finish();
+    }
+
     //endregion
 }
--- app/src/main/java/com/example/loonspartypack/MainActivity.java
+++ app/src/main/java/com/example/loonspartypack/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, TicTacToe.class));
+                finish();
             }
         });
     }
